7381515159c353301089820610eb0eb6982fff3e
wtjerry <wtjerry123@gmail.com>
2018-03-09 14:39:37 +0100

    added first Add and Push method to language

---
+++ Assembler.hs
@@ -0,0 +1,32 @@
+module Assembler where
+import Data.List
+import System.IO
+
+type State = (Int, [Int])
+
+data Instr = Add {- (a b -- a + b) -}
+           | And {- (a b -- a /\ b) -}
+           | Or {- (a b -- a \/ b) -}
+           | Not {- (a -- 1 if a=0 else 0) -}
+           | Lt {- (a b -- a < b) -}
+           | Eq {- (a b -- a = b) -}
+           | Pop {- (a -- ) -}
+           | Push Int {- ( -- a) a is the Int -}
+           | Swap {- (a b -- b a) -}
+           | Dup {- (a -- a a) -}
+           | Over {- (a b c -- a b c a) -}
+           | RotL {- (a b c -- b c a) -}
+           | Read {- ( -- a ) a is read from console in base 10 -}
+           | Print {- (a -- ) a is printed to console in base 10 -}
+           | JmpIf {- (cond addr -- ) pc := addr if cond =/= 0 -}
+           | Store {- (addr val -- ) mem[addr] := val -}
+           | Load {- (addr -- mem[addr]) -}
+           | Done deriving (Eq, Show)
+
+step :: State -> Instr -> State
+step (pc, l:r:stack) Add = (pc+1, l+r : stack)
+step (pc, stack) (Push i) = (pc+1, i : stack)
+
+run :: [Instr] -> State -> State
+run [] s = s
+run (i:is) s = run is (step s i)
---
+++ README.md
@@ -0,0 +1,7 @@
+An experiment to implement a assembler like language with Haskell.
+Highly influenced by https://github.com/beala/symbolic
+
+Try out
+``` haskell
+run [Add, (Push 3), Add] (0, [1,2])
+```
